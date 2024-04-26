99975b04cbe3bc91169b3e9725f75d063257bb3a
Rayhan Mahmud <rayhan.mahmud@brainstation-23.com>
2024-04-26 12:32:38 +0600

    Revert "Updated my code"
    This reverts commit eb10e7e98bbbfdccecb36e491a4c266a4c5ca778.

--- calculator.cpp
+++ calculator.cpp
@@ -1,11 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int division(int num1,int num2){
-    return num1/num2;
-}
-
-
 int substract(int number1, int number2){
     return (number1 - number2);
 }
@@ -13,4 +8,38 @@ int substract(int number1, int number2){
 int main(){
     int num1,num2;
     cin>>num1>>num2;
+
+    cout<<"Enter number 1: ";
+    cin>>num1;
+    cout<<"\nEnter number 2: ";
+    cin>>num2;
+    
+    char operation;
+    cout<<"\nEnter Operator: ";
+    cin>>operation;
+    cout<<endl;
+
+    switch{
+        case('+'){
+            addition(num1,num2);
+            break;
+        }
+        case('-'){
+            subsitutioin(num1,num2);
+            break;
+        }
+        case('*'){
+            multiplication(num1,num2);
+            break;
+        }
+        case('/'){
+            division(num1,num2);
+            break;
+        }
+        default:
+            cout<<"Invalid operation"<<endl;
+
+    }
+    return 0;
+
 }
